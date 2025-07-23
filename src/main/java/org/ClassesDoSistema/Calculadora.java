f420ec978996af2900c8a28a1fafe4af68572a96
Heron Lima <heronoliveira@ufba.br>
2025-07-23 07:42:58 -0300

    Base commit

---
+++ src/main/java/org/ClassesDoSistema/Calculadora.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Calculadora {
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public boolean e_par(int number) {
+        return number % 2 == 0;
+    }
+}
